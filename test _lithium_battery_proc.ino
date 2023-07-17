a6a03ce3ebce82ce8ba52042ea6895ca0fe55811
Anton Bocherov <32817153+Xomjkbood@users.noreply.github.com>
2023-07-17 20:25:47 +0300

    test _lithium_battery_proc.ino

---
+++ test _lithium_battery_proc.ino
@@ -0,0 +1,24 @@
+void setup() 
+{
+  Serial.begin(9600);
+}
+void loop()
+{
+  String lowbat;
+  String simproc;
+  float voltage = analogRead(A0) / 1024.0 * 9.0;
+  float vol = voltage * 100;
+  int voltproc = map(vol, 270, 420, 0, 100);
+  if (vol > 270) {
+    lowbat = voltproc;
+    simproc = " %";
+  }
+  else{
+      lowbat = "Low Battery"; 
+  }
+  Serial.print(lowbat);
+  Serial.println(simproc);
+  Serial.print(voltage);
+  Serial.println(" v");
+  delay(1000);        
+}
